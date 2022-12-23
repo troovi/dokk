3bbdb6e5c48d76173034d3c113fbd258f6a727e1
troovi <troovi@icloud.com>
2022-12-24 02:59:11 +0300

    docker

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Docke app UPDATE 2");
+  res.send("Docke app UPDATE 2 OOO");
 });
 
 app.listen(3000, () => {
