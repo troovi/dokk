a8d68fb393d90d8ea4d05cfcbcd4f0eae3565931
troovi <95082945+troovi@users.noreply.github.com>
2022-12-24 03:10:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Docke app PUSHED");
+  res.send("Docke app CHANGES");
 });
 
 app.listen(3000, () => {
